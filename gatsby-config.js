1302038441f056571fa8840e8d77ae5ad32825c4
Daher <dahermora91@gmail.com>
2020-05-07 22:57:54 -0400

    feat: Install and configure prettier and eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,35 @@
 module.exports = {
-  pathPrefix: "/",
+  pathPrefix: '/',
   siteMetadata: {
-    title: `Covid DashBoard`,
+    title: 'Covid DashBoard',
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    'gatsby-plugin-eslint',
+    {
+      resolve: 'gatsby-gatsby-plugin-prettier-eslint-eslint',
+      options: {
+        prettier: {
+          patterns: [
+            // the pattern "**/*.{js,jsx,ts,tsx}" is not used because we will rely on `eslint --fix`
+            '**/*.{css,scss,less}',
+            '**/*.{json,json5}',
+            '**/*.{graphql}',
+            '**/*.{md,mdx}',
+            '**/*.{html}',
+            '**/*.{yaml,yml}',
+          ],
+        },
+        eslint: {
+          patterns: '**/*.{js,jsx,ts,tsx}',
+          customOptions: {
+            fix: true,
+            cache: true,
+          },
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
