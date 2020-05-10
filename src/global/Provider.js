5939eb797c91f119455bd8064eb49800d4824ba0
Daher <dahermora91@gmail.com>
2020-05-10 10:19:48 -0400

    feat: Create Provider for shared state in app

---
+++ src/global/Provider.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+export const myContext = React.createContext();
+
+const Provider = ({ children }) => {
+  const [sharedState, setSharedState] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`https://covid19cubadata.github.io/data/covid19-cuba.json?_=1589076786848`)
+      .then(response => response.json())
+      .then(resultData => {
+        setSharedState(resultData);
+      });
+  }, []);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, [sharedState]);
+
+  return isLoading ? <div>Loading</div> : <myContext.Provider value={{ sharedState }}>{children}</myContext.Provider>;
+};
+
+export default ({ element }) => <Provider>{element}</Provider>;
