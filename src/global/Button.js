6dd128d4aee203c6c7107130b06eb1fe9feda9fa
Alexander Gonzalez <alexfertel97@gmail.com>
2020-05-12 13:12:58 -0400

    build: Introduce rambda

--- src/global/Button.js
+++ src/global/Button.js
@@ -1,43 +1,38 @@
 /* eslint-disable react/button-has-type */
 import React from 'react';
-import { createClasses, getClasses } from './utils';
+import always from 'ramda/src/always';
+import cond from 'ramda/src/cond';
+import equals from 'ramda/src/equals';
+import T from 'ramda/src/T';
 
-const variants = {
-  outlined: 'outlined',
-  text: 'text',
-  inverted: 'inverted',
-  contained: 'contained',
-};
-
-const sizes = {
-  small: 'small',
-  medium: 'medium',
-  large: 'large',
-};
-
+const variants = ['outlined', 'text', 'inverted', 'contained']
+const sizes = ['small', 'medium', 'large']
 const variantClasses = [
-  createClasses(variants.outlined, 'text-purple-500 bg-gray-100 border-2 border-purple-500'),
-  createClasses(variants.text, 'text-purple-500'),
-  createClasses(variants.inverted, 'text-purple-600 bg-purple-200'),
-];
-const defaultVariantClass = 'text-gray-100 bg-purple-600';
-
-const getVariantClasses = variant => getClasses(variant, variantClasses, defaultVariantClass);
+  'text-purple-500 bg-gray-100 border-2 border-purple-500',  // outlined
+  'text-purple-500',  // text
+  'text-purple-600 bg-purple-200',  // inverted
+  'text-gray-100 bg-purple-600'  // contained
+]
 
 const hoverClasses = [
-  createClasses(variants.outlined, 'hover:text-gray-100 hover:bg-purple-500 hover:border-transparent'),
-  createClasses(variants.text, 'hover:bg-purple-100 hover:text-purple-600'),
-  createClasses(variants.inverted, 'hover:bg-purple-100'),
-];
-const defaultVariantHover = 'hover:bg-purple-400';
+  'hover:text-gray-100 hover:bg-purple-500 hover:border-transparent',
+  'hover:bg-purple-100 hover:text-purple-600',
+  'hover:bg-purple-100',
+  'hover:bg-purple-400',
+]
 
-const getHoverClasses = variant => getClasses(variant, hoverClasses, defaultVariantHover);
+const sizeClasses = [
+  'px-3 py-2',
+  'px-4 py-3',
+  'px-5 py-4',
+]
 
-const sizesClasses = [createClasses(sizes.small, 'px-3 py-2'), createClasses(sizes.small, 'px-5 py-4')];
+const createMatches = (ids, matches) =>
+  cond(matches.map((match, i) => (ids[i] ? [equals(ids[i]), always(match)] : [T, match[i]])));
 
-const defaultSizeClasses = 'px-4 py-3';
-
-const getSizeClasses = size => getClasses(size, sizesClasses, defaultSizeClasses);
+const getVariantClasses = createMatches(variants, variantClasses);
+const getHoverClasses = createMatches(variants, hoverClasses);
+const getSizeClasses = createMatches(sizes, sizeClasses);
 
 const Button = props => {
   const { variant = 'contained', size = 'medium', children, onClick = () => {}, type = 'button', classes = '' } = props;
