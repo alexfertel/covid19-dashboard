e3917d5cd09dd04baddacd2f8325f25937a78837
Alexander Gonzalez <alexfertel97@gmail.com>
2020-05-12 17:28:24 -0400

    clean: Use createMatcher in Typography, clean-up and document code

--- src/global/Button.js
+++ src/global/Button.js
@@ -1,48 +1,52 @@
 /* eslint-disable react/button-has-type */
 import React from 'react';
-import always from 'ramda/src/always';
-import cond from 'ramda/src/cond';
-import equals from 'ramda/src/equals';
-import T from 'ramda/src/T';
+import { createMatcher } from './utils';
 
-const variants = ['outlined', 'text', 'inverted', 'contained']
-const sizes = ['small', 'medium', 'large']
+const variants = ['outlined', 'text', 'inverted', 'contained'];
+const sizes = ['small', 'medium', 'large'];
+
 const variantClasses = [
-  'text-purple-500 bg-gray-100 border-2 border-purple-500',  // outlined
-  'text-purple-500',  // text
-  'text-purple-600 bg-purple-200',  // inverted
-  'text-gray-100 bg-purple-600'  // contained
-]
+  'text-purple-500 bg-gray-100 border-2 border-purple-500', // outlined
+  'text-purple-500', // text
+  'text-purple-600 bg-purple-200', // inverted
+  'text-gray-100 bg-purple-600', // contained
+];
 
 const hoverClasses = [
-  'hover:text-gray-100 hover:bg-purple-500 hover:border-transparent',
-  'hover:bg-purple-100 hover:text-purple-600',
-  'hover:bg-purple-100',
-  'hover:bg-purple-400',
-]
+  'hover:text-gray-100 hover:bg-purple-500 hover:border-transparent', // outline
+  'hover:bg-purple-100 hover:text-purple-600', // text
+  'hover:bg-purple-100', // inverted
+  'hover:bg-purple-400', // contained
+];
 
-const sizeClasses = [
-  'px-3 py-2',
-  'px-4 py-3',
-  'px-5 py-4',
-]
+const sizeClasses = ['px-3 py-2', 'px-4 py-3', 'px-5 py-4'];
+const baseClasses =
+  'rounded-2xl inline-block items-center justify-between font-semibold focus:outline-none focus:shadow-outline';
 
-const createMatches = (ids, matches) =>
-  cond(matches.map((match, i) => (ids[i] ? [equals(ids[i]), always(match)] : [T, match[i]])));
+const getVariantClasses = createMatcher(variants, variantClasses);
+const getHoverClasses = createMatcher(variants, hoverClasses);
+const getSizeClasses = createMatcher(sizes, sizeClasses);
 
-const getVariantClasses = createMatches(variants, variantClasses);
-const getHoverClasses = createMatches(variants, hoverClasses);
-const getSizeClasses = createMatches(sizes, sizeClasses);
+const Button = ({
+  children,
+  variant = 'contained',
+  size = 'medium',
+  onClick = () => {},
+  type = 'button',
+  classes = '',
+  className = ''
+}) => {
+  const computedClasses = [
+    baseClasses,
+    getVariantClasses(variant),
+    getSizeClasses(size),
+    getHoverClasses(variant),
+  ].join(' ');
 
-const Button = props => {
-  const { variant = 'contained', size = 'medium', children, onClick = () => {}, type = 'button', classes = '' } = props;
-  const baseClasses =
-    'rounded-2xl inline-block items-center justify-between font-semibold focus:outline-none focus:shadow-outline';
-
-  const className =
-    classes || [baseClasses, getVariantClasses(variant), getSizeClasses(size), getHoverClasses(variant)].join(' ');
+  const formalClasses = [classes, computedClasses].join(' ');
+  const appliedClasses = className || formalClasses;
   return (
-    <button className={className} onClick={onClick} type={type}>
+    <button className={appliedClasses} onClick={onClick} type={type}>
       {children}
     </button>
   );
--- src/global/Typography.js
+++ src/global/Typography.js
@@ -1,45 +1,27 @@
 import React from 'react';
-import { createClasses, getClasses } from './utils';
+import { createMatcher } from './utils';
 
-const variants = {
-  h1: 'h1',
-  h2: 'h2',
-  h3: 'h3',
-  h4: 'h4',
-  h5: 'h5',
-  h6: 'h6',
-  subtitle1: 'subtitle1s',
-  subtitle2: 'subtitle2',
-  body1: 'body1',
-  body2: 'body2',
-  buttonText: 'buttonText',
-  captionText: 'captionText',
-  overLineText: 'overLineText',
-};
+const headings = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+const regular = ['subtitle1', 'subtitle2', 'body1', 'body2', 'buttonText', 'captionText', 'overLineText'];
 
-const defaultVariantClass = '';
-const typographyVariants = [
-  createClasses(variants.h1, 'text-5xl font-quicksand'),
-  createClasses(variants.h2, 'text-4xl font-quicksand'),
-  createClasses(variants.h3, 'text-3xl font-quicksand'),
-  createClasses(variants.h4, 'text-2xl font-quicksand'),
-  createClasses(variants.h5, 'text-xl font-quicksand'),
-  createClasses(variants.h6, 'text-lg font-quicksand'),
-  createClasses(variants.subtitle1, 'text-lg leading-loose font-nunito'),
-  createClasses(variants.subtitle2, 'text-md leading-normal text-gray-800 font-nunito'),
-  createClasses(variants.body1, 'text-lg leading-tight font-semibold font-nunito'),
-  createClasses(variants.body2, 'text-md leading-none font-semibold text-gray-800 font-nunito'),
-  createClasses(variants.buttonText, 'text-md leading-normal font-semibold font-nunito'),
-  createClasses(variants.captionText, 'text-sm leading-tight font-semibold font-nunito'),
-  createClasses(variants.overLineText, 'text-md font-thin leading-loose font-semibold font-nunito'),
-];
+const headingClasses = ['5xl', '4xl', '3xl', '2xl', 'xl', 'lg'].map(cls => `text-${cls} font-quicksand`);
+const regularClasses = [
+  'text-lg leading-loose',
+  'text-md leading-normal text-gray-800',
+  'text-lg leading-tight font-semibold',
+  'text-md leading-none font-semibold text-gray-800',
+  'text-md leading-normal font-semibold',
+  'text-sm leading-tight font-semibold',
+  'text-md font-thin leading-loose font-semibold',
+].map(cls => `${cls} font-nunito`);
 
-const getVariantClasses = variant => getClasses(variant, typographyVariants, defaultVariantClass);
+const getHeadingClasses = createMatcher(headings, headingClasses);
+const getRegularClasses = createMatcher(regular, regularClasses);
 
-const Typography = props => {
-  const { variant = variants.body1, children, classes = '' } = props;
-  const className = [getVariantClasses(variant), classes].join(' ');
-  return <p className={className}>{children}</p>;
+const Typography = ({ children, variant = regular[0], classes = '', className = '' }) => {
+  const formalClasses = [classes, getHeadingClasses(variant) || getRegularClasses(variant)].join(' ');
+  const appliedClasses = className || formalClasses;
+  return <p className={appliedClasses}>{children}</p>;
 };
 
 export default Typography;
