af1014d91ff8ae24c8c6b5f76cb70a5d1b6c8ea4
Alexander Gonzalez <alexfertel97@gmail.com>
2020-05-15 08:29:59 -0400

    fix: Center loader horizontally and vertically, resize it and position it absolutly

--- src/global/Loader.js
+++ src/global/Loader.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Loader = ({ size = 64 }) => (
-  <div className="justify-center">
-    <div className={`w-${size} h-${size} ease-linear border-8 border-t-8 border-gray-200 rounded-full  loader`} />
+const Loader = ({ size = 32 }) => (
+  <div className="absolute inset-0 flex items-center justify-center">
+    <div className={`w-${size} h-${size} ease-linear border-8 border-t-8 border-gray-200 rounded-full loader`} />
   </div>
 );
 
