3bc87d47a22c7a01cadffafe8194138dcae71c85
Daher <dahermora91@gmail.com>
2020-05-18 20:28:34 -0400

    feat: Add real data to Daily section component

--- src/components/home/DailySection.js
+++ src/components/home/DailySection.js
@@ -1,32 +1,114 @@
-import React from 'react';
+/* eslint-disable no-use-before-define */
+import React, { useState, useEffect } from 'react';
 
 import Card from '../Card';
 
-const info = [
-  { title: 'Diagnosticados', number: 1862, delta: { color: 'text-red-2', text: '+ 22' } },
-  { title: 'Activos', number: 321, delta: { color: 'text-blue-2', text: '- 13' } },
-  { title: 'Evacuados', number: 2, delta: { color: 'text-gray-3', text: '+ 0' } },
-  { title: 'Fallecidos', number: 79, delta: { color: 'text-blue-2', text: '+ 0' } },
-  { title: 'Recuperados', number: 1460, delta: { color: 'text-blue-2', text: '+ 35' } },
-];
+const DailySection = ({ cases = [] }) => {
+  const [info, setInfo] = useState([]);
 
-const DailySection = () => (
-  <Card className="">
-    <div className="flex w-full pt-1 pl-3 pr-2 divide-x divide-gray-2">
-      {info.map(({ title, number, delta }, i) => (
-        <div
-          key={title}
-          className={`${i !== 0 ? 'pl-8' : ''} ${
-            i !== info.length - 1 ? 'pr-8 w-48' : 'pr-4'
-          } font-medium leading-5 font-quicksand`}
-        >
-          <h2 className="text-gray-4">{title}</h2>
-          <p className="mt-2 text-3xl font-semibold tracking-wide text-black">{number}</p>
-          <p className={`mt-2 text-xs ${delta.color}`}>{delta.text}</p>
-        </div>
-      ))}
-    </div>
-  </Card>
-);
+  useEffect(() => {
+    if (cases && cases.length > 0) {
+      const { diagnosed, activeCases, evacuated, deceased, recovered } = getAllAccumulatedCases(cases);
+      const { lastDiagnosed, lastActiveCases, lastEvacuated, lastDeceased, lastRecovered } = getLastCases(cases);
+      setInfo([
+        {
+          title: 'Diagnosticados',
+          quantity: diagnosed,
+          delta: { color: getTextColor(false, lastDiagnosed), text: getQuantityString(lastDiagnosed) },
+        },
+        {
+          title: 'Recuperados',
+          quantity: recovered,
+          delta: { color: getTextColor(true, lastRecovered), text: getQuantityString(lastRecovered) },
+        },
+        {
+          title: 'Activos',
+          quantity: activeCases,
+          delta: { color: getTextColor(false, lastActiveCases), text: getQuantityString(lastActiveCases) },
+        },
+        {
+          title: 'Fallecidos',
+          quantity: deceased,
+          delta: { color: getTextColor(false, lastDeceased), text: getQuantityString(lastDeceased) },
+        },
+        {
+          title: 'Evacuados',
+          quantity: evacuated,
+          delta: { color: 'text-gray-3', text: getQuantityString(lastEvacuated) },
+        },
+      ]);
+    }
+  }, [cases]);
+
+  return (
+    <Card className="">
+      <div className="flex w-full pt-1 pl-3 pr-2 divide-x divide-gray-2">
+        {info.map(({ title, quantity, delta }, i) => (
+          <div
+            key={title}
+            className={`${i !== 0 ? 'pl-8' : ''} ${
+              i !== info.length - 1 ? 'pr-8 w-48' : 'pr-4'
+            } font-medium leading-5 font-quicksand`}
+          >
+            <h2 className="text-gray-4">{title}</h2>
+            <p className="mt-2 text-3xl font-semibold tracking-wide text-black">{quantity}</p>
+            <p className={`mt-2 text-xs ${delta.color}`}>{delta.text}</p>
+          </div>
+        ))}
+      </div>
+    </Card>
+  );
+};
+
+// #region  Queries
+
+const getAllAccumulatedCases = cases =>
+  cases.reduce(
+    (accumulator, current) => {
+      const { diagnosed, evacuated, deceased, recovered } = current.day;
+      const newDiagnosed = accumulator.diagnosed + diagnosed.length;
+      const newEvacuated = accumulator.evacuated + evacuated;
+      const newDeceased = accumulator.deceased + deceased;
+      const newRecovered = accumulator.recovered + recovered;
+      const newActiveCases = newDiagnosed - newEvacuated - newDeceased - newRecovered;
+      return {
+        diagnosed: newDiagnosed,
+        activeCases: newActiveCases,
+        evacuated: newEvacuated,
+        deceased: newDeceased,
+        recovered: newRecovered,
+      };
+    },
+    {
+      diagnosed: 0,
+      activeCases: 0,
+      evacuated: 0,
+      deceased: 0,
+      recovered: 0,
+    }
+  );
+
+const getLastCases = cases => {
+  const lastDay = cases[cases.length - 1].day;
+  const { diagnosed, evacuated, deceased, recovered } = lastDay;
+  return {
+    lastDiagnosed: diagnosed.length,
+    lastActiveCases: diagnosed.length - evacuated - deceased - recovered,
+    lastEvacuated: evacuated,
+    lastDeceased: deceased,
+    lastRecovered: recovered,
+  };
+};
+
+// #endregion
+
+const getTextColor = (isPositiveText, quantity) => {
+  if (isPositiveText) {
+    return quantity >= 0 ? 'text-blue-2' : 'text-red-2';
+  }
+  return quantity > 0 ? 'text-red-2' : 'text-blue-2';
+};
+
+const getQuantityString = quantity => `${quantity >= 0 ? '+' : ''}${quantity}`;
 
 export default DailySection;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,27 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
 import SEO from '../components/Seo';
 import DailySection from '../components/home/DailySection';
 import CalendarSection from '../components/home/CalendarSection';
 import CaseDistributionSection from '../components/home/CaseDistributionSection';
 import MapSection from '../components/home/MapSection';
+import { storageContext } from '../global/Provider';
 
-const IndexPage = () => (
-  <>
-    <SEO title="Dashboard | Home" />
-    <div className="w-full min-h-full p-8">
-      <div className="flex w-full">
-        <DailySection />
-        <CalendarSection />
+const IndexPage = () => {
+  const { cases } = useContext(storageContext);
+  return (
+    <>
+      <SEO title="Dashboard | Home" />
+      <div className="w-full min-h-full p-8">
+        <div className="flex w-full">
+          <DailySection cases={cases} />
+          <CalendarSection />
+        </div>
+        <div className="flex w-full">
+          <CaseDistributionSection />
+          <MapSection />
+        </div>
       </div>
-      <div className="flex w-full">
-        <CaseDistributionSection />
-        <MapSection />
-
-      </div>
-    </div>
-  </>
-);
-
+    </>
+  );
+};
 export default IndexPage;
