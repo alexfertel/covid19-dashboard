f89a64b8e755265290b964c99b16aecd8bc72c9c
Daher <dahermora91@gmail.com>
2020-05-26 18:09:56 -0400

    feat: Create Gis template page

---
+++ src/pages/gis.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import SEO from '../components/Seo';
+import Typography from '../global/Typography';
+import MapContainer from '../components/Map/MapContainer';
+
+const GisPage = () => (
+  <Layout>
+    <SEO title="Gis" />
+    <div className="w-full pb-5 pl-20">
+      <Typography variant="h1">GIS</Typography>
+    </div>
+
+    <MapContainer
+      center={[21.860263, -79.628906]}
+      position={[21.860263, -79.628906]}
+      zoom={6}
+      markerText="Hello, this is a marker"
+    />
+  </Layout>
+);
+
+export default GisPage;
