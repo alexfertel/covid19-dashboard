3cc1d457b2bb18e9499310a406d8a08adef79907
Daher <dahermora91@gmail.com>
2020-05-26 17:58:07 -0400

    feat: Add map functions to utils

--- src/global/utils.js
+++ src/global/utils.js
@@ -14,9 +14,51 @@ import equals from 'ramda/src/equals';
 // matcher(1)  // Number 1
 // matcher(2)  // Number 2
 // matcher(3)  // Number 3
-// matcher(4)  // undefined 
+// matcher(4)  // undefined
 
-const createMatcher = (patterns, rules) =>
-  cond(patterns.map((pattern, i) => ([equals(pattern), always(rules[i])])));
+const createMatcher = (patterns, rules) => cond(patterns.map((pattern, i) => [equals(pattern), always(rules[i])]));
 
-export { createMatcher };
+//* Map functions
+const CreateRanges = (min, max, numberOfRanges) => {
+  const result = [];
+  const step = Math.floor(max / numberOfRanges);
+  let currentMin = min;
+
+  for (let index = 0; index < numberOfRanges; index++) {
+    if (currentMin > max) {
+      break;
+    }
+    result.push({ min: currentMin, max: currentMin + step > max ? max : currentMin + step });
+    currentMin += step + 1;
+  }
+  return result;
+};
+
+const ConvertRangeToString = ({ min, max }) => `${min}-${max}`;
+
+// ! Do it as binary Search!
+const getIndexRangeFromNumber = (number, bucketRanges = []) =>
+  bucketRanges.findIndex(range => number >= range.min && number <= range.max);
+
+const getScaledColor = (current, range, treatZeroDifferent = true) => {
+  const zeroColor = '#FFEDA0';
+  const colorScale = ['#FED976', '#FEB24C', '#FD8D3C', '#FC4E2A', '#E31A1C', '#BD0026', '#800026'];
+  return treatZeroDifferent && current === 0 ? zeroColor : colorScale[getIndexRangeFromNumber(current, range)];
+};
+
+const fullColorScale = ['#FFEDA0', '#FED976', '#FEB24C', '#FD8D3C', '#FC4E2A', '#E31A1C', '#BD0026', '#800026'];
+
+// *Mock up functions
+const percentRandom = () => Math.random().toFixed(2);
+const randomInteger = factor => Math.floor(Math.random() * factor);
+
+export {
+  createMatcher,
+  percentRandom,
+  randomInteger,
+  fullColorScale,
+  getScaledColor,
+  ConvertRangeToString,
+  CreateRanges,
+  getIndexRangeFromNumber,
+};
