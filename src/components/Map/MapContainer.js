6c8424477313715e042f519398ab96baeb67ec65
Alexander Gonzalez <alexfertel97@gmail.com>
2020-05-27 10:08:35 -0400

    format: Fix MapContainer code formatting

--- src/components/Map/MapContainer.js
+++ src/components/Map/MapContainer.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic';
 
-const LeafletMap = dynamic(
-  () => import('./LeafletMap'),
-  { ssr: false }
-)
-
+const LeafletMap = dynamic(() => import('./LeafletMap'), { ssr: false });
 
 const MapContainer = props => <LeafletMap {...props} />;
 
